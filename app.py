631586c50e7ce75e44c176b1f9ffeb3d0c29a7a0
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-29 02:52:19 +0300

    added dotenv

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import pymongo
 import json
 from bson.objectid import ObjectId
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from data.config import CONFIG
 
 
@@ -12,7 +15,7 @@ app = Flask(__name__)
 mongo connection
 """
 try:
-    CONNECTION_STRING = 'localhost:27017' if CONFIG.flask_env == 'development' else "mongodb+srv://" + CONFIG.db_username + ":" + CONFIG.db_password + "@cluster0.izwr5.mongodb.net/?retryWrites=true&w=majority"
+    CONNECTION_STRING = 'mongodb://localhost:27017/recipe' if CONFIG.flask_env == 'development' else f'mongodb+srv://{CONFIG.db_username}:{CONFIG.db_password}@cluster0.izwr5.mongodb.net/?retryWrites=true&w=majority'
     mongo = pymongo.MongoClient(CONNECTION_STRING)
     db = mongo.recipe # database name
     mongo.server_info() # trigger exception if cannot connect to db
