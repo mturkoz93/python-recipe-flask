e00d025d0f48693539f21d48c96c866e827b59e5
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-28 20:20:25 +0300

    updated home end point

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return 'Welcome adessi - ' + CONFIG.is_prod + CONFIG.flask_env
+    try:
+        return 'Welcome adessi - ' + CONFIG.is_prod + CONFIG.flask_env
+    except:
+        return 'There is an erro1'
 
 if (__name__ == '__main__'):
     app.run(debug=True)
