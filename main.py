23b178a5ac4ea0e07053e4f06bb6dc2da4c3a68f
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-28 19:40:03 +0300

    testing env

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return 'Welcome adessi'
+    is_prod = os.environ.get('IS_HEROKU', None)
+    return 'Welcome adessi - ' + is_prod
 
 if (__name__ == '__main__'):
     app.run(debug=True)
