5cbe87063e712ffe6e120bef2be8246d665dd71f
Mustafa Turkoz <mturkoz93@gmail.com>
2022-06-29 03:19:45 +0300

    update dotenv

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask, Response, request
 import pymongo
 import json
 from bson.objectid import ObjectId
 
-from dotenv import load_dotenv
-load_dotenv()
+
+if not os.environ.get("PRODUCTION"):
+    from dotenv import load_dotenv
+    load_dotenv()
 
 from data.config import CONFIG
 
